12c10da7378e35f00e8a2e869f0d7c0b183ce613
Julian-Enable <elabc80@gmail.com>
2023-08-29 20:13:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,62 +13,61 @@ drawSquares(actualRow);
 listenInput(actualRow);
 
 // Enfocar en el primer input
-addFocus(actualRow)
+addFocus(actualRow);
 
 function listenInput(actualRow) {
-    // Seleccionar todos los inputs y convertirlos en un array
-    let squares = actualRow.querySelectorAll(".square");
-    squares = [...squares];
+  // Seleccionar todos los inputs y convertirlos en un array
+  let squares = actualRow.querySelectorAll(".square");
+  squares = [...squares];
 
-    // Array para almacenar las letras ingresadas por el usuario
-    let userInput = [];
+  // Array para almacenar las letras ingresadas por el usuario
+  let userInput = [];
 
-    // Agregar un evento de escucha para cada input
-    squares.forEach((element, index) => {
-        element.addEventListener("input", (event) => {
-        // Almacenar la letra ingresada por el usuario en mayúsculas
-        userInput.push(event.target.value.toUpperCase());
-        console.log(userInput);
+  // Agregar un evento de escucha para cada input
+  squares.forEach((element, index) => {
+    element.addEventListener("input", (event) => {
+      // Almacenar la letra ingresada por el usuario en mayúsculas
+      userInput.push(event.target.value.toUpperCase());
+      console.log(userInput);
 
-        // Cambiar el enfoque al siguiente input si existe
-        if (event.target.nextElementSibling) {
-            event.target.nextElementSibling.focus();
-        } else {
-            // Comparar las letras ingresadas con las letras de la palabra original
-            let rightIndex = compareArrays(wordArray, userInput);
-            console.log(rightIndex);
+      // Cambiar el enfoque al siguiente input si existe
+      if (event.target.nextElementSibling) {
+        event.target.nextElementSibling.focus();
+      } else {
+        // Comparar las letras ingresadas con las letras de la palabra original
+        let rightIndex = compareArrays(wordArray, userInput);
+        console.log(rightIndex);
 
-            // Resaltar los inputs correctos en verde
-            rightIndex.forEach((element) => {
-            squares[element].classList.add("green");
-            });
-
-            // si los arreglos son iguales
-            if (rightIndex.length == wordArray.length) {
-            resultElement.innerHTML = `<p>Ganaste!</p>
-                    <button class="button">Reiniciar</button>`;
-                    let resetBtn = document.querySelector('.button')
-                    resetBtn.addEventListener('click', () =>{
-                        location.reload();
-                    }); 
-                    return;
-            }
-            //cambiar estilos si existe la letra pero no esta en la posicion correcta
-            let existIndexArray = existLetter(wordArray, userInput);
-            existIndexArray.forEach((element) => {
-            squares[element].classList.add("gold");
-            });
-
-            //crear una nueva fila
-            let actualRow = createRow()
-            drawSquares(actualRow)
-            listenInput(actualRow)
-            addFocus(actualRow)
-
-        }
+        // Resaltar los inputs correctos en verde
+        rightIndex.forEach((element) => {
+          squares[element].classList.add("green");
         });
+
+        // si los arreglos son iguales
+        if (rightIndex.length == wordArray.length) {
+          showResult("Ganaste");
+
+          let resetBtn = document.querySelector(".button");
+          resetBtn.addEventListener("click", () => {
+            location.reload();
+          });
+          return;
+        }
+        //cambiar estilos si existe la letra pero no esta en la posicion correcta
+        let existIndexArray = existLetter(wordArray, userInput);
+        existIndexArray.forEach((element) => {
+          squares[element].classList.add("gold");
+        });
+
+        //crear una nueva fila
+        let actualRow = createRow();
+        drawSquares(actualRow);
+        listenInput(actualRow);
+        addFocus(actualRow);
+      }
     });
-    }
+  });
+}
 
 //FUNCIONES
 
@@ -97,16 +96,16 @@ function existLetter(array1, array2) {
 }
 function createRow() {
   rowId++;
-  if (rowId <= 5) { 
+  if (rowId <= 5) {
     let newRow = document.createElement("div");
     newRow.classList.add("row");
     newRow.setAttribute("id", rowId);
     ainContainer.appendChild(newRow);
     return newRow;
-}
-    
+  } else {
+    showResult("Perdiste");
   }
-  
+}
 
 function drawSquares(actualRow) {
   // Crear los inputs para cada letra en el array
@@ -119,8 +118,13 @@ function drawSquares(actualRow) {
   });
 }
 
-function addFocus(actualRow){
-    let focusElement = actualRow.querySelector(".focus");
-    console.log("focusElement");
-    focusElement.focus();
+function addFocus(actualRow) {
+  let focusElement = actualRow.querySelector(".focus");
+  console.log("focusElement");
+  focusElement.focus();
+}
+
+function showResult(textMsg) {
+  resultElement.innerHTML = `<p>${textMsg}</p>
+  <button class="button">Reiniciar</button>`;
 }
